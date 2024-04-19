3832453197514c0d0936c49e86cfc48c74b487ab
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-04-18 22:48:11 -0700

    exploratory data analysis

--- scripts/1_eda.jl
+++ scripts/1_eda.jl
@@ -4,19 +4,62 @@
 
 # Preliminaries -------------------------------------------------------------------
 
+# Activate the package environment
+
+using Pkg
+
+Pkg.activate("final_env")
+
 # Load packages
 
 using TidierData
+using DataFrames
 using TidierFiles
 
 # Load data 
 
 # Calibration data
 
-calibration_raw = read_csv("data/calibration/All estimation raw data.csv")
+calibration_raw = @chain read_csv("data/calibration/All estimation raw data.csv") begin
+        @clean_names()
+        @arrange(set)
+    end
 
 # Exploratory data analysis (EDA) -------------------------------------------------------
 
 # Glimpse
 
 @glimpse(calibration_raw)
+
+# How many observations (rows) per subjectid 
+
+obs_per_subject = @chain calibration_raw begin
+    @group_by(subjid)
+    @summarize(n_obs = n())
+    @arrange(n_obs)
+end
+
+# Count how many sets per subjid (should be just 1)
+
+sets_per_subject = @chain calibration_raw begin
+    @group_by(subjid, set)
+    @summarize(n_sets = n())
+    @ungroup()
+    @select(-n_sets)
+    @group_by(subjid)
+    @summarize(n_sets = n())
+end
+
+# Get set 1 data 
+
+set1_df = @chain calibration_raw begin
+    @filter(set == 1)
+end
+
+# Unique values of the GameID variable
+
+@chain set1_df begin
+    @select(gameid) 
+    @distinct()
+    @arrange(gameid)
+end
