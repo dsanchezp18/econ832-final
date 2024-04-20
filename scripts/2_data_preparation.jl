6b61580b50b8ec1635f4abbeb9e375c4ec09cfa3
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-04-19 19:59:59 -0700

    prepare data set

--- scripts/2_data_preparation.jl
+++ scripts/2_data_preparation.jl
@@ -30,3 +30,16 @@ calibration_raw = @chain read_csv("data/calibration/All estimation raw data.csv"
 # Data preparation -------------------------------------------------------------------
 
 # Prepare data for DFNNs
+# Categorical variables should be made into binary variables (manual one-hot encoding)
+
+cleaned_calibration_df = @chain calibration_raw begin
+    @mutate(subjid = as_string(subjid),
+            location_rehovot = if_else(location == "Rehovot", 1, 0),
+            gender_female = if_else(gender == "F", 1, 0))
+end
+
+# Exporting -------------------------------------------------------------------
+
+# Export the cleaned calibration data
+
+write_csv(cleaned_calibration_df, "data/output/cleaned_calibration.csv")
